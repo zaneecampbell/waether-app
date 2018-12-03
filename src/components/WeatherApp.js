008ba9c03923befd854c40b5f1d81e7c53b69a44
Zane Campbell <zanegitinit@gmail.com>
2018-12-03 14:10:31 -0800

    correct information displayed, functionally set, styling needed

---
+++ src/components/WeatherApp.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+export class WeatherApp extends React.Component {
+  state = {
+    city: '',
+    country: '',
+    latitiude: '',
+    longitude: '',
+    weather: []
+  }
+
+  componentWillMount() {
+    this.getWeather();
+  }
+
+  getWeather = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        latitiude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${this.state.latitiude}&lon=${this.state.longitude}&units=imperial&APPID=f4cc91ca7ae413816afbd1648484b1bd`)
+        .then((response) => {
+          return response.json();
+        })
+          .then((myJson) => {
+            const relevantWeather = []
+
+            // Gets every 8th entry so we only have 5 days of weather not every 3 hours worth of 5 days
+            myJson.list.map((weatherInfo, idx) => {
+              if (idx % 8 === 0) {
+                relevantWeather.push(weatherInfo)
+              } else {
+                // do nothing
+              }
+            })
+
+            // Takes out all the unwanted information
+            const simpleWeather = []
+            relevantWeather.forEach((day) => {
+              let weatherObject = {}
+              weatherObject.time = day.dt_txt
+              weatherObject.temp = day.main.temp
+              weatherObject.min = day.main.temp_min
+              weatherObject.max = day.main.temp_max
+              weatherObject.sky = day.weather[0].main
+              weatherObject.description = day.weather[0].description
+              simpleWeather.push(weatherObject)
+            });
+
+            this.setState({
+              city: myJson.city.name,
+              country: myJson.city.country,
+              weather: simpleWeather
+            });
+          })
+    });
+  }
+
+  handleCheat = () => {
+    console.log(this.state.weather)
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.weather.length === 0 ? (
+            <div>
+              Loading... 
+            </div>
+          ) : (
+              <Grid spacing={40} container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              >
+                {
+                  this.state.weather.map((day, idx) => (
+                    <Grid item key={idx}>
+                      <Paper>
+                        {day.time}
+                        {day.temp}
+                        {day.min}
+                        {day.max}
+                        {day.sky}
+                        {day.description}
+                      </Paper>
+                    </Grid>
+                  ))
+                }
+              </Grid>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+export default WeatherApp;
+
+
+// api key f4cc91ca7ae413816afbd1648484b1bd
+
+
+// dt_txt
+// main.temp, main.temp_min, main.temp_max
+// weather[0].main, weather[0].description
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import DashboardPage from '../components/DashboardPage';
+import WeatherApp from '../components/WeatherApp';
 import NotFoundPage from '../components/NotFoundPage';
-import LoginPage from '../components/LoginPage'
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
@@ -13,8 +12,7 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <PublicRoute path="/" component={LoginPage} exact={true}/>
-        <PrivateRoute path="/dashboard" component={DashboardPage}/>
+        <Route component={WeatherApp} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
